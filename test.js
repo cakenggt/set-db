e5daa0a33e0b07895dcc2dc91baa5ea74cebf685
Alec Lownes <alownes@wescodist.com>
2017-04-24 10:26:34 -0400

    Format tests

--- test.js
+++ test.js
@@ -1,63 +1,52 @@
-require("leaked-handles");
-const SetDB = require('./index');
+require('leaked-handles'); // eslint-disable-line import/no-unassigned-import
 const test = require('tape');
+const SetDB = require('./index');
 
 const network = 'test';
 
 test('put dbs', t => {
-  var db1 = new SetDB(network);
-  var db2 = new SetDB(network);
+	const db1 = new SetDB(network);
+	const db2 = new SetDB(network);
 
-  t.plan(2);
-  t.deepEqual(db1.db, db2.db);
-  db1.put({
-    _id: '1',
-    name: 'testname'
-  });
-  db2.on('sync', () => {
-    t.deepEqual(db1.db, db2.db);
-    db1.disconnect();
-    db2.disconnect();
-  });
+	t.plan(2);
+	t.deepEqual(db1.db, db2.db);
+	db1.put({
+		_id: '1',
+		name: 'testname'
+	});
+	db2.on('sync', () => {
+		t.deepEqual(db1.db, db2.db);
+		db1.disconnect();
+		db2.disconnect();
+	});
 });
 
 test('load db', t => {
-  var db = new SetDB(network, {
-    dbHash: 'QmPVfQJ4yjgwz2XQESBjRmJmYZYjJdYW2bd61jUMAqis6V'
-  });
-  t.plan(1);
-  db.on('ready', () => {
-    t.deepEqual(db.db, {
-      '1': {
-        _id: '1',
-        name: 'testname'
-      }
-    });
-    db.disconnect();
-  });
+	const db = new SetDB(network, {
+		dbHash: 'QmPVfQJ4yjgwz2XQESBjRmJmYZYjJdYW2bd61jUMAqis6V'
+	});
+	t.plan(1);
+	db.on('ready', () => {
+		t.deepEqual(db.db, {
+			1: {
+				_id: '1',
+				name: 'testname'
+			}
+		});
+		db.disconnect();
+	});
 });
 
 test('sync dbs', t => {
-  var db1 = new SetDB(network, {
-    dbHash: 'QmPVfQJ4yjgwz2XQESBjRmJmYZYjJdYW2bd61jUMAqis6V'
-  });
-  var db2 = new SetDB(network);
+	const db1 = new SetDB(network, {
+		dbHash: 'QmPVfQJ4yjgwz2XQESBjRmJmYZYjJdYW2bd61jUMAqis6V'
+	});
+	const db2 = new SetDB(network);
 
-  t.plan(1);
-  db2.on('sync', () => {
-    t.deepEqual(db1.db, db2.db);
-    db1.disconnect();
-    db2.disconnect();
-  });
+	t.plan(1);
+	db2.on('sync', () => {
+		t.deepEqual(db1.db, db2.db);
+		db1.disconnect();
+		db2.disconnect();
+	});
 });
-
-/*
-var db = new SetDB('test', {
-  dbHash: 'QmZ6BstVjmMqryBZ25adBSwiT2jwPKu1ek4xZKHU4TL8zG'
-});
-
-db.put({
-  _id: '3',
-  name: 'test'
-});
-*/
